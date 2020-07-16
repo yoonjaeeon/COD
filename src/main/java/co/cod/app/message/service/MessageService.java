a2a79227c42ee4c7e359987448eb16a8d94b2cfd
hjw <cupi12@naver.com>
2020-07-16 14:20:01 +0900

    0716

--- src/main/java/co/cod/app/message/service/MessageService.java
+++ src/main/java/co/cod/app/message/service/MessageService.java
@@ -8,5 +8,5 @@ public interface MessageService {
 	public List<MessageVO> messageList(String adminId); //메세지 전체 조회 (adminId에 따른)
 	public MessageVO getMessage(Integer MessageSeq);   //메세지 단건조회(클릭시)
 	public void insertMessage(MessageVO messageVO);   //메세지 보내기
-	public void updateMessage(Integer messageSeq);    //메세지 읽음 표시
+	public void updateMessage(Integer messageSeq);    //메세지 읽음 표시	
 }

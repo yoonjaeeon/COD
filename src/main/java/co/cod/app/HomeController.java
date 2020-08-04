25ae155814ebb9d2deb7e99fd67730675afee4fc
hjw <cupi12@naver.com>
2020-08-04 16:13:29 +0900

    하준원 빠갈나기 전

--- src/main/java/co/cod/app/HomeController.java
+++ src/main/java/co/cod/app/HomeController.java
@@ -2,12 +2,16 @@ package co.cod.app;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import co.cod.app.member.service.MemberService;
+
 @Controller
 public class HomeController {
+	@Autowired MemberService memberService;
    
    @RequestMapping("home")
    public String home(Model model) {
